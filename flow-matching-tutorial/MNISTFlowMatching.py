957fa77ffddbabddc2d5f7eb70c75c48c3ea2f43
leoposc <schmid.leopold@web.de>
2026-09-16 16:28:48 +0200

    add transformer w/ FM impl for MNIST gen.

--- flow-matching-tutorial/MNISTFlowMatching.py
+++ flow-matching-tutorial/MNISTFlowMatching.py
@@ -1,14 +1,24 @@
 import torch 
 from torch import nn, Tensor 
 import torch.nn.functional as F
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Subset
 from torchvision import datasets, transforms
 from typing import override 
 import matplotlib.pyplot as plt 
+from enum import Enum
+
+class Mode(Enum):
+    CNN = 1
+    BasicTransformer = 2
+
+MODE = Mode.CNN
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
+# ================================================================
+# Version 1: CNN Architecture
+# ================================================================
 class FlowMatching(nn.Module):
     def __init__(self) -> None:
         super().__init__()
@@ -57,6 +67,97 @@ class FlowMatching(nn.Module):
 
 
 # ================================================================
+# version 2: transformer architecture
+# ================================================================
+class FlowMatching_with_Transformer(nn.Module):
+    def __init__(self):
+        super().__init__() 
+
+        self.embedding_dim=32
+
+        self.digit_embedding = nn.Embedding(
+            num_embeddings=10,
+            embedding_dim=self.embedding_dim
+        )
+
+        self.time_embedding = nn.Sequential(
+            nn.Linear(1, self.embedding_dim),
+            nn.SiLU(),
+            nn.Linear(self.embedding_dim, self.embedding_dim),
+        )
+
+        self.patch_embedding = nn.Conv2d(
+            in_channels=1,
+            out_channels=self.embedding_dim,
+            kernel_size=4,
+            stride=4,
+        )
+
+        self.pos_embedding = nn.Parameter(
+            torch.randn(1, 49, self.embedding_dim)
+        )
+
+        self.num_layers = 4
+        self.layers = nn.ModuleList([
+            nn.TransformerEncoderLayer(
+                d_model=self.embedding_dim,
+                nhead=4,
+                dim_feedforward=128,
+                batch_first=True
+            )
+            for _ in range(self.num_layers)
+        ])
+
+        self.output_proj = nn.Linear(
+            self.embedding_dim, 
+            4 * 4,
+        )
+
+
+
+    @override
+    def forward(self, x, t, digit):
+
+        digit_emb = self.digit_embedding(digit)
+        digit_emb = digit_emb.unsqueeze(1)
+
+        time_emb = self.time_embedding(t.unsqueeze(1)).unsqueeze(1)
+
+        patch = self.patch_embedding(x)
+        patch = patch.flatten(2)
+        patch = patch.transpose(1, 2)
+
+        image_tokens = patch + self.pos_embedding
+
+        x = torch.cat([
+            digit_emb, 
+            time_emb,
+            image_tokens,
+            ], dim=1
+        )
+
+        for layer in self.layers:
+            x = layer(x)
+
+
+        # remove digit and time tokens again 
+        image_tokens = x[:, 2:, :]
+
+        # convert each token back into a 4x4 patch 
+        patches = self.output_proj(image_tokens)
+        patches = patches.transpose(1, 2) # back to [B, 16, 49]
+
+        # reconstruct the velocity image 
+        velocity = F.fold(
+            patches, 
+            output_size=(28, 28),
+            kernel_size=4,
+            stride=4,
+        ) # [B, 1, 28, 28]
+
+        return velocity
+
+# ================================================================
 # Training
 # ================================================================
 
@@ -72,13 +173,21 @@ dataset = datasets.MNIST(
     transform=transform,
 )
 
+indices = torch.randperm(len(dataset))[:4096]
+dataset = Subset(dataset, indices)
+
 loader = DataLoader(
     dataset,
-    batch_size=64,
+    batch_size=128,
     shuffle=True,
 )
 
-model = FlowMatching().to(device)
+
+match MODE:
+    case Mode.CNN:
+        model = FlowMatching().to(device)
+    case Mode.BasicTransformer:
+        model = FlowMatching_with_Transformer().to(device)
 
 optimizer = torch.optim.Adam(
     model.parameters(),
@@ -86,7 +195,7 @@ optimizer = torch.optim.Adam(
 )
 
 
-for epoch in range(10):
+for epoch in range(50):
 
     total_loss = 0 
 
@@ -96,7 +205,7 @@ for epoch in range(10):
         digit = digit.to(device)
 
         B = x1.shape[0]
-        x0 = torch.rand_like(x1)
+        x0 = torch.randn_like(x1) # gausssian source distribution
 
         t = torch.rand(B, device=device)
 
@@ -142,7 +251,7 @@ def generate(model, digit, steps=10):
             (B,),
             i / steps,
             device=device
-        )
+        ) # Creates tensor of batch_size with the current time step
 
         velocity = model(
             x,
@@ -158,7 +267,7 @@ def generate(model, digit, steps=10):
 digits = torch.arange(10, device=device)
 
 samples = generate(
-    model, digits, steps=20
+    model, digits, steps=50
 )
 
 
